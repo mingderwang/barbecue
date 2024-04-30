0c0a019402d45ee8412ee9afba3b438c96a3aee1
mingderwang <admin@ming-ders-MacBook.local>
2024-04-30 20:51:06 +0800

    use audoroute

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,20 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import { autoroutes } from 'elysia-autoroutes'
 import { swagger } from '@elysiajs/swagger'
 
-const { Client, Presets } = require('userop')
-const { InfuraProvider, Signer, Wallet, parseEther } = require("ethers")
-
-const plugin = new Elysia({ name: 'plugin' })
-    .macro(({ onBeforeHandle }) => ({
-        hi(word: string) {
-            onBeforeHandle(() => {
-                console.log(word)
-            })
-        }
-    }))
-
 const app = new Elysia()
-    .use(swagger())
-    .use(plugin)
-    .get('/', () => 'hi', {
-        hi: 'Elysia'
+  .use(swagger())
+  .use(
+    autoroutes({
+      routesDir: "./routes", // -> optional, defaults to './routes'
+      prefix: "/smart_account", // -> optional, defaults to ''
+      generateTags: false, // -> optional, defaults to true
     })
-    .get('/id/:id', ({ params: { id } }) => id)
-
-app.listen(3000);
+  )
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
 
-export type App = typeof app
+export type ElysiaApp = typeof app
